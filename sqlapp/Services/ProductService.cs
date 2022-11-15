168d66bc4be1d8a10c881b7f348f32100abd48e2
t-ffotsotall <t-ffotsotall@DESKTOP-PMM74TB>
2022-11-15 12:01:58 +0100

    Add project files.

---
+++ sqlapp/Services/ProductService.cs
@@ -0,0 +1,52 @@
+﻿using sqlapp.Models;
+using System.Data.SqlClient;
+
+namespace sqlapp.Services
+{
+
+    // This service will interact with our Product data in the SQL database
+    public class ProductService
+    {
+    
+
+        private SqlConnection GetConnection()
+        {
+            
+            var _builder = new SqlConnectionStringBuilder();
+            _builder.DataSource = "webappserver2500.database.windows.net";
+            _builder.UserID = "franck";
+            _builder.Password = "FOtsotalla2003";
+            _builder.InitialCatalog = "webappdb";
+            return new SqlConnection(_builder.ConnectionString);
+        }
+        public List<Product> GetProducts()
+        {
+            List<Product> _product_lst = new List<Product>();
+            string _statement = "SELECT ProductID,ProductName,Quantity from Products";
+            SqlConnection _connection = GetConnection();
+            
+            _connection.Open();
+            
+            SqlCommand _sqlcommand = new SqlCommand(_statement, _connection);
+            
+            using (SqlDataReader _reader = _sqlcommand.ExecuteReader())
+            {
+                while (_reader.Read())
+                {
+                    Product _product = new Product()
+                    {
+                        ProductID = _reader.GetInt32(0),
+                        ProductName = _reader.GetString(1),
+                        Quantity = _reader.GetInt32(2)
+                    };
+
+                    _product_lst.Add(_product);
+                }
+            }
+            _connection.Close();
+            return _product_lst;
+        }
+
+    }
+}
+
